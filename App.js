9f99da50af87a4098cb82db1452b3a68005aefc4
nonpawitJT <ni.nonpawit_st@tni.ac.th>
2022-12-21 13:35:42 +0700

    icon header button

--- App.js
+++ App.js
@@ -1,68 +1,26 @@
-import { View, Text, Button ,StyleSheet } from "react-native";
-import React from "react";
-import { NavigationContainer } from "@react-navigation/native";
-import { createNativeStackNavigator } from "@react-navigation/native-stack";
-import HomeScreen from "./screens/HomeScreen";
-import AboutScreen from "./screens/AboutScreen";
-import HomePost from "./screens/HomePost";
-import CreatePost from "./screens/CreatePost";
-import FirstPage from "./pages/FirstPage";
-import SecondPage from "./pages/SecondPage";
-import ThirdPage from  "./pages/ThirdPage";
+import { View, Text,Button } from 'react-native'
+import React from 'react'
 
-const Stack = createNativeStackNavigator();
+import {createDrawerNavigator} from'@react-navigation/drawer'
+import {NavigationContainer} from '@react-navigation/native'
 
+
+function HomeScreen (){
+  return(
+    <View style ={{flex:1,alignItems:'center',justifyContent:'center'}}>
+      <Button title='Go to Notification'/>
+    </View>
+  );
+}
+const Drawer = createDrawerNavigator();
 const App = () => {
   return (
-    <NavigationContainer>
-      <Stack.Navigator 
-        initialRouteName="FirstPage"
-        screenOptions={{
-          headerStyle:{
-            backgroundColor:"#8abaae"
-          },
-          headerTintColor:"#FFFFFF",
-          headerTitleStyle:{
-            fontWeight:'bold'
-          }
-        }}
-        >
-        {/* <Stack.Screen name="Home" component={HomeScreen} />
-        <Stack.Screen name="About" component={AboutScreen} /> */}
-        {/* <Stack.Screen name="HomePost" component={HomePost} />
-        <Stack.Screen name="CreatePost" component={CreatePost} /> */}
-        <Stack.Screen name="FirstPage" component={FirstPage} />
-        <Stack.Screen name="SecondPage" component={SecondPage} />
-        <Stack.Screen name="ThirdPage" component={ThirdPage} />
-      </Stack.Navigator>
-    </NavigationContainer>
-  );
-};
+ <NavigationContainer>
+<Drawer.Navigator useLegacyImplementation>
+<Drawer.Screen name="Home" component={HomeSreen}/>
+</Drawer.Navigator>
+ </NavigationContainer>
+  )
+}
 
-export default App;
-
-
-
-
-
-
-
-// function HomeScreen({ navigation }) {
-  // return (
-  //   <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
-  //     <Text>Home Screen</Text>
-      // <Button 
-      //   title="About Me" 
-      //   onPress={() => navigation.navigate("About")} 
-      //   />
-  //   </View>
-  // );
-// }
-
-// function AboutScreen() {
-//   return (
-//     <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
-//       <Text>About Screen</Text>
-//     </View>
-//   );
-// }
+export default App
